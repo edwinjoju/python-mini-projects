6e298c2d3146a5e6e3034f0e25adb00a5f113ff9
Joju <ejoju@deloitte.com>
2024-10-21 14:40:29 +0530

    updated password_generator file

--- password_generator.py
+++ password_generator.py
@@ -1,36 +1,40 @@
 import string
 import secrets
 
+#check uppercase
 def check_upper(password):
     for char in password:
         if char.isupper():
             return True
     return False
 
+#check symbols
 def check_symbol(password):
     for char in password:
         if char in string.punctuation:
             return True
     return False
 
-def generate_password(length,symbol,uppercase):
-    combination =string.ascii_lowercase+string.digits
+def generate_password(length,upper,symbol):
 
-    if symbol:
-        combination+=string.punctuation
-    
-    if uppercase:
+    combination = string.ascii_lowercase+string.digits # combination contain a to z & 0 to 9
+
+    if upper: #if upper is true adds uppercase to combination
         combination+=string.ascii_uppercase
 
-    combination_length = len(combination)
+    if symbol:#if symbol is true adds symbols to combination
+        combination+=string.punctuation
+
+    combination_length=len(combination)
+
     new_password=''
 
-    for x in range(length):
+    for x in range(length): #randomly takes value from combination and adds to new_password
         new_password+=combination[secrets.randbelow(combination_length)]
-    
+
     return new_password
 
-if __name__ == '__main__':
+if __name__=='__main__':
     for x in range(6):
-        password = generate_password(length=5,symbol=True,uppercase=True)
-        print(f'{password} | uppercase: {check_upper(password)} | symbol: {check_symbol(password)}')
+        password = generate_password(5,symbol=True, upper=True)
+        print(f'{password} | Upper:{check_upper(password)} | Symbol:{check_symbol(password)}')
